10a6bb9b6fe13ba1cc84ce5dcb6b77187d1b5bb3
Peter <peter.ford@hq.acm.org>
2015-02-11 23:25:40 -0500

    1st commit

---
+++ API_V4/src/main/java/com/acm/v4/controllers/TestController.java
@@ -0,0 +1,21 @@
+package com.acm.v4.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping(value="test")
+public class TestController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value="/world", method=RequestMethod.GET)
+	public String test() {
+		return "login";
+	}
+	
+}
